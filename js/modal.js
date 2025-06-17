7a731ea8fe8a3479273a5db9c02221c7cf24a43c
Dina <79221383+YermakovaDina@users.noreply.github.com>
2025-06-17 15:31:18 +0300

    basic version
    основная базовая информация сайта

---
+++ js/modal.js
@@ -0,0 +1,21 @@
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector('[data-modal-open]'),
+      closeModalBtn: document.querySelector('[data-modal-close]'),
+      modal: document.querySelector('[data-modal]'),
+      mobileMenu:document.querySelector('.menu')
+    };
+  
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+    refs.mobileMenu.addEventListener('click', closeModal);
+  
+    function toggleModal() {
+      document.body.classList.toggle("menu-open");
+      refs.modal.classList.toggle('is-hidden');
+    }
+    function closeModal() {
+          document.body.classList.toggle("menu-open");
+      refs.modal.classList.add('is-hidden');
+      }
+  })();
